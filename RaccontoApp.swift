e3772ac1d6f18d5bcd780e2ddcd9501f2047cdc0
downeve <downeve@gmail.com>
2026-05-05 16:27:29 +0200

    Initial commit: Fixed project structure

---
+++ RaccontoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaccontoApp.swift
+//  Racconto
+//
+//  Created by 최다운 on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct RaccontoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
